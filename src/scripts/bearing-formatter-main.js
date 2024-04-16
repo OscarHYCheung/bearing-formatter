63719de50baa3ddf6bed2e9c5845f2bc8bcbeed9
Oscar Cheung <github@oscarcheung.me>
2024-04-16 16:15:14 +0800

    Add line numbers to the textareas
    And the scrolling are now sync for the input and output textareas.

--- src/scripts/bearing-formatter-main.js
+++ src/scripts/bearing-formatter-main.js
@@ -1,8 +1,11 @@
 (() => {
     const inputTextArea = document.querySelector("#bearing-formatter-input");
     const outputTextArea = document.querySelector("#bearing-formatter-output");
+    const inputLineNumbers = document.querySelector("#input-line-numbers");
+    const outputLineNumbers = document.querySelector("#output-line-numbers");
 
     const inputRegex = /^\s*(\d+|\.\d+|\d+\.\d*)\s*$/;
+    let currentLineCount = 1;
     const convertBearing = (inputStr) => {
         inputStr = inputStr.trim();
         if (!inputStr) {
@@ -40,17 +43,63 @@
         return `${input}%%d${(minute < 10 ? "0" + minute : minute)}'${(second < 10 ? "0" + second : second)}"`;
     };
 
+    const alignLineNumbers = () => {
+        const lineNumberWidth = inputLineNumbers.clientWidth;
+        inputTextArea.style.paddingLeft = `${lineNumberWidth}px`;
+        outputTextArea.style.paddingLeft = `${lineNumberWidth}px`;
+    }
+    alignLineNumbers();
+
     const convert = () => {
         const inputText = inputTextArea.value;
         const inputs = inputText.split("\n");
         const outputs = inputs.map((inputRow) => {
             return convertBearing(inputRow);
         });
+        const newLineCount = inputs.length;
+        if (newLineCount > currentLineCount) {
+            const fragment = document.createDocumentFragment();
+            for (let i = currentLineCount; i < newLineCount; i++) {
+                const lineNumberSpan = document.createElement("span");
+                lineNumberSpan.innerHTML = `${i + 1}:`;
+                fragment.appendChild(lineNumberSpan);
+            }
+            inputLineNumbers.appendChild(fragment.cloneNode(true));
+            outputLineNumbers.appendChild(fragment);
+        } else if (newLineCount < currentLineCount) {
+            const inputParent = inputLineNumbers.parentNode;
+            const outputParent = outputLineNumbers.parentNode;
+            inputParent.removeChild(inputLineNumbers);
+            outputParent.removeChild(outputLineNumbers);
+            for (let i = currentLineCount; i > newLineCount; i--) {
+                inputLineNumbers.removeChild(inputLineNumbers.lastChild);
+                outputLineNumbers.removeChild(outputLineNumbers.lastChild);
+            }
+            inputParent.appendChild(inputLineNumbers);
+            outputParent.appendChild(outputLineNumbers);
+        }
+        currentLineCount = newLineCount;
+        alignLineNumbers();
 
         const outputText = outputs.join("\n");
         outputTextArea.value = outputText;
     }
     inputTextArea.addEventListener("input", convert);
+
+    inputTextArea.addEventListener("scroll", () => {
+        requestAnimationFrame(() => {
+            inputLineNumbers.scrollTop = inputTextArea.scrollTop;
+            outputLineNumbers.scrollTop = outputTextArea.scrollTop;
+            outputTextArea.scrollTop = inputTextArea.scrollTop;
+        });
+    }, { passive: true });
+    outputTextArea.addEventListener("scroll", () => {
+        requestAnimationFrame(() => {
+            inputLineNumbers.scrollTop = outputTextArea.scrollTop;
+            inputTextArea.scrollTop = outputTextArea.scrollTop;
+            outputLineNumbers.scrollTop = outputTextArea.scrollTop;
+        });
+    }, { passive: true });
 
     let toastTimer = null;
     let enlargeTimer = null;
